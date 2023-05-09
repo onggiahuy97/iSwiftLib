407a72a41e3549b9238a59b59d615e3fc887d80e
Huy Ong <ogh.huy@gmail.com>
2023-05-09 07:40:56 -0700

    First

---
+++ Sources/iSwiftLib/Binding+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Huy Ong on 4/28/23.
+//
+
+import SwiftUI
+
+extension Binding {
+    func onChange(_ handler: @escaping () -> Void) -> Binding<Value> {
+        Binding {
+            return self.wrappedValue
+        } set: { newValue in
+            self.wrappedValue = newValue
+            handler()
+        }
+
+    }
+}
---
+++ Sources/iSwiftLib/View+Extensions.swift
@@ -0,0 +1,53 @@
+//
+//  File.swift
+//  
+//
+//  Created by Huy Ong on 5/4/23.
+//
+
+import SwiftUI
+
+struct FilteringList<T: Identifiable, Content: View>: View {
+    @State private var filteredItems = [T]()
+    @State private var filterString = ""
+    
+    let listItems: [T]
+    let filterKeyPaths: [KeyPath<T, String>]
+    let content: (T) -> Content
+    
+    var body: some View {
+        VStack {
+            TextField("Filter", text: $filterString.onChange {
+                withAnimation {
+                    applyFilter()
+                }
+            })
+            .textFieldStyle(.roundedBorder)
+            .padding(.horizontal)
+            
+        List(filteredItems, rowContent: content)
+                .onAppear(perform: applyFilter)
+        }
+    }
+    
+    
+    init(_ data: [T], filterKeys: KeyPath<T, String>..., @ViewBuilder rowContent: @escaping (T) -> Content) {
+        listItems = data
+        filterKeyPaths = filterKeys
+        content = rowContent
+    }
+    
+    private func applyFilter() {
+        let cleanedFilter = filterString.trimmingCharacters(in: .whitespacesAndNewlines)
+        if cleanedFilter.isEmpty {
+            filteredItems = listItems
+        } else {
+            filteredItems = listItems.filter { element in
+                filterKeyPaths.contains {
+                    element[keyPath: $0]
+                        .localizedCaseInsensitiveContains(cleanedFilter)
+                }
+            }
+        }
+    }
+}
